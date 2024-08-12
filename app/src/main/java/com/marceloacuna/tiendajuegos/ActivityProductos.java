397fd9f8dd564d527d4e4239f9055f7a8f333331
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-08-11 23:17:21 -0400

    se crea login y productos

--- app/src/main/java/com/marceloacuna/tiendajuegos/ActivityProductos.java
+++ app/src/main/java/com/marceloacuna/tiendajuegos/ActivityProductos.java
@@ -1,57 +1,24 @@
 package com.marceloacuna.tiendajuegos;
 
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-       getMenuInflater().inflate(R.menu.menu_options,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-
-            return super.onOptionsItemSelected(item);
-
-    }
-
-
-
+public class ActivityProductos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_productos);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
-
 }
