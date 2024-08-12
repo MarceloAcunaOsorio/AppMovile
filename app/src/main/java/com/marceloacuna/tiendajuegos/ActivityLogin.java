c4ff724e1667baed5c51837881632937db07b914
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-08-11 20:45:03 -0400

    se crea login

--- app/src/main/java/com/marceloacuna/tiendajuegos/ActivityLogin.java
+++ app/src/main/java/com/marceloacuna/tiendajuegos/ActivityLogin.java
@@ -1,6 +1,10 @@
 package com.marceloacuna.tiendajuegos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,22 @@ public class ActivityLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameText = findViewById(R.id.username_input);
+        passwordText = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+
+                // Realiza alguna acción con el nombre de usuario
+                Toast.makeText(ActivityLogin.this,"Nombre de usuario :"+username+"\n Contraseña: "+password,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/marceloacuna/tiendajuegos/MainActivity.java
+++ app/src/main/java/com/marceloacuna/tiendajuegos/MainActivity.java
@@ -3,6 +3,9 @@ package com.marceloacuna.tiendajuegos;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +16,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
